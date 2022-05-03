7563ad137b2ca354c38802ea93422cf1b6bc3865
Sayoko123f <63059711+Sayoko123f@users.noreply.github.com>
2022-05-03 11:13:56 +0800

    Set build config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
-})
+    plugins: [vue()],
+    base: 'https://sayoko123f.github.io/guess-1a2b/',
+    build: {
+        outDir: 'docs',
+    },
+});
